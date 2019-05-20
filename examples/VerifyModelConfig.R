4367c12e1fd60db2151e1220535a58460d9bf825
Adrian Carretero <acchavez@ucsd.edu>
2019-05-20 14:06:06 -0700

    Added example config file for VerifyModel.R

---
+++ examples/VerifyModelConfig.R
@@ -0,0 +1,10 @@
+config <- list(
+    kboolnetPath = "",
+    rxnconPath = "",
+    driveFile = "https://docs.google.com/spreadsheets/d/1xCNwCLL5GdsZs8_dHmmACVpBfOhrun2cKwnhJa8dsk8/edit?usp=sharing",
+    modules = "", 
+    minQuality = 0,
+    out = "./out/",
+    ligands = "CCL2_[rec]--{0}"
+)
+
