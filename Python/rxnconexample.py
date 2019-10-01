b4238728ee8c332687ffb1fc724e3d3f2bb994e0
palmerito0 <sandiegobutterflies@gmail.com>
2019-10-01 10:32:44 -0700

    Example of how to get states and their corresponding reactions from a string

--- Python/rxnconexample.py
+++ Python/rxnconexample.py
@@ -8,8 +8,9 @@
 #             interactions with other componenets (eg Gai_[Gbg]--Gbg_[Gai]) or self interactions (idk how those work though)
 from rxncon.input.excel_book.excel_book import ExcelBook
 from rxncon.visualization.regulatory_graph import RegulatoryGraph
+from rxncon.core.state import state_from_str
 
-excel_book = ExcelBook("/home/adrian/Internship/CCR2_rxncon/CCR2_master/CCR2_rxncon_master_2019_07_05.xlsx")
+excel_book = ExcelBook("/home/adrian/Internship/CCR2_rxncon/CCR2_master/out/master.xlsx")
 rxncon_system = excel_book.rxncon_system
 
 # The states and components lists hold objects of type Spec
@@ -21,7 +22,7 @@ print(states[0])
 print(components[0])
 
 # You can pick these out by name using their "name" attribute (list comprehension woo)
-print([state for state in states if (state.name == "CCR2_[Gai]--0" or state.name == "Pyk2_[(P)]-{{p}}")])
+print([state for state in states if (state.name == "CCR2_[Ga]--0" or state.name == "Pyk2_[(P)]-{{p}}")])
 print([component for component in components if (component.name == "AC" or component.name == "CCL2")])
 
 # You can see which states "live" on a component (i.e. which states involve a given component)
@@ -33,5 +34,18 @@ print(states[0].components)
 
 # You can also see the mutually exclusive states for a given state
 print(rxncon_system.complement_states(states[0]))
+
+# You can get fully expanded state(s) from a string
+orig_state = state_from_str("Gai--0") 
+expanded_states = [state for state in states if state.is_subset_of(orig_state)]
+print(expanded_states)
+
+# You can get the reactions that produce a given state
+producing_reactions = [reaction for reaction in rxncon_system.reactions if any([state.is_subset_of(orig_state) for state in reaction.produced_states])]
+print(producing_reactions)
+
+# You can see which reactions are bidirectional using the BIDIRECTIONAL_REACTIONS variable
+from rxncon.core.reaction import BIDIRECTIONAL_REACTIONS
+print(BIDIRECTIONAL_REACTIONS)
 
 # There's definitely more stuff in the rxncon source files, let me know if you want me to try and dig some new stuff up!
