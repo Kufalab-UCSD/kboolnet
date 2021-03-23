393cb8982009a332cf8dcce4bfbc9accfea8945f
Willow <sandiegobutterflies@gmail.com>
2021-03-23 01:48:56 -0400

    Fixed example files

--- inst/scripts/examples/CXCR4_toy_NFSim/RunNFSimConfig.R
+++ inst/scripts/examples/CXCR4_toy_NFSim/RunNFSimConfig.R
@@ -1,7 +1,5 @@
 config <- list(
-    BNGPath = "~/BioNetGen-2.5.2", # Insert your own install directory here
-    # file = "./CXCR4_toy.xlsx",
-    driveFile = "https://docs.google.com/spreadsheets/d/1QbbEsxxu9FIMx1kHuXCPrKEAzPpU4xxKI5QxHTA5gEg/edit#gid=981789074",
+    file = "./CXCR4_toy.xlsx",
     ligands = "CXCL12",
     inhib = "",
     time = 5,
--- inst/scripts/examples/RasRafMekErk/RunNFSimConfig.R
+++
@@ -1,9 +0,0 @@
-config <- list(
-    BNGPath = "~/BioNetGen-2.5.2", # Insert your own install directory here
-    file = "./RasRafMekErk_master.xlsx",
-    ligands = "EGF",
-    inhib = "",
-    time = 50,
-    out = "./NFsim_out"
-)
-
