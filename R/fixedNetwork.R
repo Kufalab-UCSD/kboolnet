a680980486a032f3c5f6ad3fa249ba5751fb6031
palmerito0 <sandiegobutterflies@gmail.com>
2021-07-09 17:44:24 -0400

    fixedNetwork can now either be component or regex based

--- R/fixedNetwork.R
+++ R/fixedNetwork.R
@@ -9,28 +9,38 @@
 # Dependencies: BoolNet
 ##############################################
 
-fixedNetwork <- function(network, inhib, names, symbols, effect = 0) {
+fixedNetwork <- function(network, inhib, names, symbols, effect = 0, regex = FALSE) {
   if (!effect %in% c(0, 1)) {
     stop("Invalid effect on the network. Must be either 0 to fix off or 1 to fix on.")
   }
   inhibNodes <- character()
   for (i in 1:length(inhib)) {
-    # Make sure the inhibited nodes/components exist
-    if (!(any(grepl(paste0("^", inhib[i], "(_.*--0|_.*-\\{0\\}|)$"), names)))) {
-      stop("No neutral state found for inhibited node/component ", inhib[i], ". Please verify that ", inhib[i], " is a valid node/component in the rxncon system.")
-    } 
-    
-    # Find which nodes the inhibition corresponds to
-    inhibRegex <- paste0("(", c(paste0(inhib[i], "_.*--.*"), paste0(".*--", inhib[i], "_.*"),
-                              paste0("^", inhib[i], "$"), paste0(inhib[i], "_.*-\\{.*\\}")), ")", collapse="|")
+    # Try to match to a node
+    if (!regex) {
+      # Make sure the inhibited nodes/components exist
+      if (!(any(grepl(paste0("^", inhib[i], "(_.*--0|_.*-\\{0\\}|)$"), names)))) {
+        stop("No neutral state found for inhibited node/component ", inhib[i], ". Please verify that ", inhib[i], " is a valid node/component in the rxncon system.")
+      }
+
+      # Find which nodes the inhibition corresponds to
+      inhibRegex <- paste0("(", c(paste0(inhib[i], "_.*--.*"), paste0(".*--", inhib[i], "_.*"),
+                                paste0("^", inhib[i], "$"), paste0(inhib[i], "_.*-\\{.*\\}")), ")", collapse="|")
+    } else {
+      inhibRegex <- escapeRegex(inhib[i])
+    }
+
     inhibMatch <- grepl(inhibRegex, names)
     inhibNodes <- c(inhibNodes, symbols[inhibMatch])
-    
-    cat("Inhibitor", inhib[i], "matched to node(s)", paste0(names[inhibMatch], collapse=", "), "\n")
+
+    if (effect == 0) {
+      cat("Inhibitor", inhib[i], "matched to node(s)", paste0(names[inhibMatch], collapse=", "), "\n")
+    } else if (effect == 1) {
+      cat("Activator", inhib[i], "matched to node(s)", paste0(names[inhibMatch], collapse=", "), "\n")
+    }
   }
-  
+
   # Fix inhib nodes off
   network <- BoolNet::fixGenes(network, inhibNodes, effect)
-  
+
   return(network)
 }
